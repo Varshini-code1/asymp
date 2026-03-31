a8edaf83b5dde85b48c28e8605e815f5501eaed0
Varshini-code1 <9262@humegrammar.vic.edu.au>
2026-03-31 17:35:42 +1100

    hi

---
+++ oddoccuring.py
@@ -0,0 +1,13 @@
+n=int(input("How many elements in the array? "))#7
+ar=[]
+i=1
+while i<=n:
+    x=int(input('Enter a number : '))
+    ar.append(x)
+    i+=1
+
+res=0
+for x in ar:
+    res=res^x
+
+print(res,"is odd occuring number in array",ar)
