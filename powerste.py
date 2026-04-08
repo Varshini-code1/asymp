abf20bd9db4a710daafd33880b3b859b1afa56b4
Varshini-code1 <9262@humegrammar.vic.edu.au>
2026-04-08 19:08:44 +1000

    hi

---
+++ powerste.py
@@ -0,0 +1,16 @@
+import math
+def f1(s,size):
+    psize=(int)(math.pow(2,size))
+    x=0
+    y=0
+    for x in range(psize):
+        for y in range(size):
+            if((x&(1<<y))>0):
+                print(s[y],end="")
+        print("")
+size=int(input("Enter array size: "))
+set=[]
+for i in range(size):
+    n=input("Enter an element : ")
+    set.append(n)
+f1(set,len(set))
