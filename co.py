e022b2d73939f0a8a6cadff694bfb4221c36d039
Varshini-code1 <9262@humegrammar.vic.edu.au>
2026-05-17 20:21:25 +1000

    hi

--- co.py
+++ co.py
@@ -27,3 +27,22 @@ ar = np.eye(4)   # 4x4 identity matrix
 0 0 0 1
 '''
 print(ar)
+# 6. Create an array with a range of values
+ar = np.arange(0, 10, 2)   # Values from 0 to 8 with step 2
+print(ar)
+
+# 7. Create an array with evenly spaced values
+ar = np.linspace(0, 1, 5)   # 5 values from 0 to 1 , d=(1-0)/(5-1)=1/4=0.25-> 0, 0.25, 0.5,0.75,1.
+print(ar)
+
+
+# 8. Create an empty array (uninitialized values)
+
+ar = np.empty(2)   # May contain arbitrary values
+
+print(ar)
+
+
+#arrange the elements in ascending order
+
+print(np.sort(ar))
